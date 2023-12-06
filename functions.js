91bc55680ebcc63d751cae7dcf5d4014827f3c36
gqueico <gqueico@gmail.com>
2023-12-06 20:51:13 -0300

    :memo: Parameters and arguments

--- functions.js
+++ functions.js
@@ -2,7 +2,14 @@
 
 // parameters - parâmetros
 const sum = function(number1, number2) {
-  console.log(number1 + number2)
+  let total = number1 + number2
+  return total
 }
 
-sum(2,3) // arguments - argumentos
+let number1 = 2
+let number2 = 3
+// sum(number1, number2) // arguments - argumentos
+
+console.log(`O número 1 é ${number1}`)
+console.log(`O número 2 é ${number2}`)
+console.log(`A soma é ${sum(number1, number2)}`)

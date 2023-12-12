d34829e84e2b9e3430a4f2a8a5c18260435dce1d
gqueico <gqueico@gmail.com>
2023-12-12 11:50:58 -0300

    :sparkles: Add ex02.js

---
+++ ex02.js
@@ -0,0 +1,5 @@
+alert('Iremos somar dois números');
+let numberOne = prompt('Digite o primeiro valor:');
+let numberTwo = prompt('Digite o segundo valor:');
+let result = Number(numberOne) + Number(numberTwo);
+alert(`Resultado: ${result}`);

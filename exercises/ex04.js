8f6d8b538a5c67e2135e41caf241b3c5ece83527
gqueico <gqueico@gmail.com>
2024-06-28 10:07:51 -0300

    :recycle: Refactor ex04.js

--- exercises/ex04.js
+++ exercises/ex04.js
@@ -1,17 +1,22 @@
-const studentName = prompt('Digite o nome do aluno:');
+const studentName = prompt('Digite o nome do(a) aluno(a):');
 let firstNote = prompt('Informe a primeira nota:');
 let secondNote = prompt('Informe a segunda nota:');
 let thirdNote = prompt('Informe a terceira nota:');
 
 let average = (Number(firstNote) + Number(secondNote) + Number(thirdNote)) / 3;
 
-if(average > 6) {
+if(average > 6 && average >= 10) {
   alert('Parabéns você foi aprovado!');
-} else {
+  average = average.toFixed(0)
+}else if(average > 6 ){
+  alert('Parabéns você foi aprovado!');
+  average = average.toFixed(1)
+}else {
   alert('Dê o seu melhor na prova de recuperação!');
+  average = average.toFixed(1)
 }
 
 alert(`
   Aluno: ${studentName}
-  Média: ${average.toFixed(1)}
+  Média: ${average}
 `);
